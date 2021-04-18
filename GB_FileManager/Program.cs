dc3a3340e8cb8cb7305c7f4af7718906036ad386
anvar89 <36979151+anvar89@users.noreply.github.com>
2021-04-18 22:06:58 +0500

    Реализованны команды ChangeDirectory, Delete, PageUp, PageDown

--- GB_FileManager/Program.cs
+++ GB_FileManager/Program.cs
@@ -1,32 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Runtime.InteropServices;
-using System.Text;
 
 namespace GB_FileManager
 {
     class Program
     {
-        [DllImport("kernel32.dll", ExactSpelling = true)]
-        private static extern IntPtr GetConsoleWindow();
-        private static IntPtr ThisConsole = GetConsoleWindow();
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-        private const int HIDE = 0;
-        private const int MAXIMIZE = 3;
-        private const int MINIMIZE = 6;
-        private const int RESTORE = 9;
 
-
-        static string div = "│";
 
         static void Main(string[] args)
         {
-
             Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
-            ShowWindow(ThisConsole, MAXIMIZE);
-            //Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
 
             FileManager fm = new FileManager();
 
@@ -34,16 +16,39 @@ namespace GB_FileManager
 
             while (true)
             {
-                Console.Clear();
+                //Console.Clear();
 
                 fm.Print();
 
 
-                Console.SetCursorPosition(0, 0);
-                Console.SetCursorPosition(0, Console.WindowHeight);
-
                 switch (Console.ReadKey().Key)
                 {
+                    case ConsoleKey.Delete:
+                        fm.Delete(new string[0]);
+                        break;
+
+                    case ConsoleKey.Enter:
+                        fm.GotoParentDirectory();
+                        //string s = Console.ReadLine();
+                        //fm.ChangeDirectory(s.Split());
+                        break;
+
+                    case ConsoleKey.LeftArrow:
+                        fm.RightPathActive = false;
+                        break;
+
+                    case ConsoleKey.RightArrow:
+                        fm.RightPathActive = true;
+                        break;
+
+                    case ConsoleKey.UpArrow:
+                        fm.SelectRowUp();
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        fm.SelectRowDown();
+                        break;
+
                     case ConsoleKey.F10:
                         return;
 
